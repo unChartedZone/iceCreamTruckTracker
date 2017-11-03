1a9b4033376aec2b300edf59fb12be94459487d0
Christian Valdez <christianvaldez36@hotmail.com>
2017-11-02 23:09:48 -0700

    Moved the main code in LoginDialog to InventoryManagementSystem main method This uses the nimbus look and feel, I also removed several uneeded lines of code from the LoginDialog. The Nimbus look and feel is more modern looking

--- src/inventorymangagementsystem/InventoryManagementSystem.java
+++ src/inventorymangagementsystem/InventoryManagementSystem.java
@@ -57,11 +57,43 @@ public class InventoryManagementSystem {
             }
         
 
-        LoginDialog ld=new LoginDialog();
-        ld.setLocationRelativeTo(null);
-        ld.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        ld.setVisible(true);
-        //com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel
+//        LoginDialog ld=new LoginDialog();
+//        ld.setLocationRelativeTo(null);
+//        ld.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+//        ld.setVisible(true);
+//        com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+            System.out.println("Fucking test");
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(LoginDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(LoginDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(LoginDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(LoginDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the dialog */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                LoginDialog dialog = new LoginDialog();
+                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                    @Override
+                    public void windowClosing(java.awt.event.WindowEvent e) {
+                        System.exit(0);
+                    }
+                });
+                dialog.setVisible(true);
+            }
+        });
     }
     
 }
